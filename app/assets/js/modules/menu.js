a266b2480a9fb327340753e0a763e716973688d4
Ivan <ivan.mendieta@aol.com>
2019-04-03 23:48:45 -0700

    show and hide menu accordion

--- app/assets/js/modules/menu.js
+++ app/assets/js/modules/menu.js
@@ -1,1 +1,14 @@
+//click menu icon and open menu accordion
+document.getElementById('my-Menu').addEventListener('click', () => {
 
+  //grab the accordion element
+  var accordion = document.querySelector('.menu-icon__accordion-menu');
+
+  //validate if the accordion exist add class visible else removed
+  if( accordion.classList.contains('menu-icon--visible') ){
+      accordion.classList.remove('menu-icon--visible');
+  }else{
+      accordion.classList.add('menu-icon--visible');
+  }
+
+});
